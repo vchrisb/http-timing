8728f9526f7e69de235a5eab5758dc39eb1cca96
vchrisb <vchrisb@users.noreply.github.com>
2018-02-13 12:13:33 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # http-timing
-a golang application to get detailed http timings for an endpoint for troubleshooting
+a golang application to get detailed http timings for an endpoint for troubleshooting purposes on Cloud Foundry or a different environment
+
+## Cloud Foundry deployment
+
+The application does issue a http request to the specified endpoint, adding a UUID as a query string, and will log long running requests.
+
+### prepare
+
+Copy `manifest.yml.example` to `manifest.yml` and adapt to your needs.
+
+#### variables
+`TEST_HTTP_ENDPOINT` The HTTP endpoint to be used for troubleshooting
+`TEST_THRESHOLD` The threshold in milliseconds over which a log will written to stdout
+`TEST_RATE` The rate of requests per second
+
+#### dep
+
+run `dep` to create vendoring files:
+`dep ensure`
+
+### push
+
+push the application:
+`cf push`
+
+### evaluate logs
+
+The application does write a log line with details, prepended with `CSV` when ever the threshold is reached.
+One can filter the log with this tag and write to a file. The file can then imported with `space` as a delimiter.
+`cf logs http-timing |  grep CSV  --line-buffered > app_log.csv`
+
+Similarily on the target application, e.g. [cf-helloworld](https://github.com/vchrisb/cf-helloworld), you can filter on the router response time and use the UUID to correlate a slow request with the gorouter log.
+`cf logs cf-helloworld | grep response_time --line-buffered > server_log.log`
+
+## local deployment
+
+```
+dep ensure
+go build
+export TEST_HTTP_ENDPOINT="https://google.com"
+export TEST_THRESHOLD=1000
+export TEST_RATE=1
+./http-timing
+```
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import ("github.com/tcnksm/go-httpstat"
+        "time"
+        "net/http"
+        "github.com/asaskevich/govalidator"
+        "io/ioutil"
+        "io"
+        "log"
+        "os"
+        "strconv"
+        "fmt"
+        "github.com/google/uuid"
+      )
+
+
+func main() {
+
+  endpoint := os.Getenv("TEST_HTTP_ENDPOINT")
+  if (!govalidator.IsURL(endpoint)) {
+    log.Fatal("No or malformed URL specified!")
+  }
+  var limit time.Duration =  time.Millisecond * time.Duration(3000)
+  test_limit, err := strconv.Atoi(os.Getenv("TEST_THRESHOLD"))
+  if (err == nil) {
+    limit =  time.Millisecond * time.Duration(test_limit)
+  }
+
+  var rate =  time.Second / time.Duration(1)
+  test_rate, err := strconv.Atoi(os.Getenv("TEST_RATE"))
+  if (err == nil) {
+    rate =  time.Second / time.Duration(test_rate)
+  }
+
+  fmt.Println("HTTP Endpoint: ", endpoint)
+  fmt.Println("Threshold: ", limit)
+  fmt.Println("Rate: ", rate)
+
+  throttle := time.Tick(rate)
+  i := 1
+  for i > 0 {
+    <-throttle  // rate limit our Service.Method RPCs
+    //fmt.Print(".")
+    go call(endpoint, limit, i)
+    i++
+  }
+
+}
+
+func call(endpoint string, limit time.Duration, run int) {
+
+  // generate uuid
+  id := uuid.New()
+
+  call_endpoint := endpoint + "?" + id.String()
+  req, err := http.NewRequest("GET", call_endpoint , nil)
+  if err != nil {
+      log.Fatal(err)
+  }
+
+
+  // Create go-httpstat powered context and pass it to http.Request
+  var result httpstat.Result
+  ctx := httpstat.WithHTTPStat(req.Context(), &result)
+  req = req.WithContext(ctx)
+
+  tr := &http.Transport{
+    DisableKeepAlives: true,
+  }
+  client := &http.Client {
+    Transport: tr,
+  }
+  res, err := client.Do(req)
+  if err != nil {
+      log.Fatal(err)
+  }
+
+  if _, err := io.Copy(ioutil.Discard, res.Body); err != nil {
+      log.Fatal(err)
+  }
+  res.Body.Close()
+
+  end_time := time.Now()
+  result.End(end_time)
+
+  if (result.Total(end_time) > limit) {
+    // Show results
+    fmt.Println("### Total is ", result.Total(end_time))
+    //fmt.Printf("%+v", result)
+    fmt.Println("DNSLookup: ", result.DNSLookup.Seconds())
+    fmt.Println("TCPConnection: ", result.TCPConnection.Seconds())
+    fmt.Println("TLSHandshake: ", result.TLSHandshake.Seconds())
+    fmt.Println("ServerProcessing: ", result.ServerProcessing.Seconds())
+    fmt.Println("CSV", run, id, result.DNSLookup.Seconds(), result.TCPConnection.Seconds(), result.TLSHandshake.Seconds(), result.ServerProcessing.Seconds())
+  }
+
+}
